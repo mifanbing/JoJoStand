1c6b756c07ccbc18caba27f1d4e196a1f78e5e40
zeyi wang <wang-ze-yi@hotmail.com>
2020-01-12 16:09:03 -0600

    Drop pin logic

--- JoJoStand/JoJoStand/Main/MainViewController.swift
+++ JoJoStand/JoJoStand/Main/MainViewController.swift
@@ -3,8 +3,11 @@ import UIKit
 class MainViewController: UIViewController {
     @IBOutlet weak var modelImage: UIImageView!
     @IBOutlet weak var bodyJointCollectionView: UICollectionView!
+    
     let allBodyJoints: [String] = BodyJoint.allCases.map { $0.rawValue }
-    var currentBodyIndex: Int = 0
+    var currentBodyJoint: BodyJoint!
+    var currentBodyJointIndex: Int!
+    var bodyLocations: BodyLocation = BodyLocation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -12,25 +15,49 @@ class MainViewController: UIViewController {
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
         modelImage.isUserInteractionEnabled = true
         modelImage.addGestureRecognizer(tapGestureRecognizer)
+        modelImage.image = UIImage(named: "Josuke")
         
         bodyJointCollectionView.delegate = self
         bodyJointCollectionView.dataSource = self
-        
         //cell layout
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width: 80, height: 40)
         layout.minimumInteritemSpacing = 0
         layout.minimumLineSpacing = 1
         bodyJointCollectionView.collectionViewLayout = layout
+        
+        currentBodyJoint = .head
+        currentBodyJointIndex = 0
     }
 
     @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
     {
         let tappedImage = tapGestureRecognizer.view as! UIImageView
-
         let tapLocation = tapGestureRecognizer.location(in: tappedImage)
         
-        print("\(tapLocation.x) \(tapLocation.y)")
+        let frame = tappedImage.frame
+        let xNormalized = Double(tapLocation.x / frame.width)
+        let yNormalized = Double(tapLocation.y / frame.height)
+        
+        bodyLocations.update(bodyJoint: currentBodyJoint, normalizedLocation: NormalizedLocation(xNormalized: xNormalized, yNormalized: yNormalized))
+        //print("\(xNormalized) \(yNormalized) \(currentBodyJoint)")
+        dropPin(image: tappedImage, x: tapLocation.x, y: tapLocation.y)
+    }
+    
+    func dropPin(image: UIImageView, x: CGFloat, y: CGFloat) {
+        if bodyLocations.bodyLocations[currentBodyJoint] != nil {
+            for subView in image.subviews {
+                if subView.tag == currentBodyJointIndex {
+                    subView.removeFromSuperview()
+                }
+            }
+        }
+        
+        let pinImage = UIImageView(frame: CGRect(x: x, y: y, width: CGFloat(20), height: CGFloat(20)))
+        pinImage.backgroundColor = .green
+        pinImage.tag = currentBodyJointIndex
+        
+        image.addSubview(pinImage)
     }
 }
 
@@ -38,9 +65,9 @@ extension MainViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let row = indexPath.row
         let section = indexPath.section
-        currentBodyIndex = section * 4 + row
-        
-        print(currentBodyIndex)
+        let index = section * 4 + row
+        currentBodyJoint = BodyJoint(rawValue: allBodyJoints[index])!
+        currentBodyJointIndex = index
     }
 }
 
@@ -71,22 +98,25 @@ extension MainViewController: UICollectionViewDataSource {
     
 }
 
-struct BodyLocation {
-    var bodyLocations: [BodyJoint: NormalizedLocation]
+class BodyLocation {
+    var bodyLocations: [BodyJoint: NormalizedLocation?]
  
     init() {
         var bodyLocationsTemp = [BodyJoint: NormalizedLocation]()
         BodyJoint.allCases.forEach {
-            bodyLocationsTemp[$0] = NormalizedLocation(xNormalized: 0, yNormalized: 0)
+            bodyLocationsTemp[$0] = nil //NormalizedLocation(xNormalized: 0, yNormalized: 0)
         }
         bodyLocations = bodyLocationsTemp
     }
     
-    struct NormalizedLocation {
-        let xNormalized: Double
-        let yNormalized: Double
+    func update(bodyJoint: BodyJoint, normalizedLocation: NormalizedLocation) {
+        bodyLocations[bodyJoint] = normalizedLocation
     }
-    
+}
+
+struct NormalizedLocation {
+    let xNormalized: Double
+    let yNormalized: Double
 }
 
 enum BodyJoint: String, CaseIterable {
